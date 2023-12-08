f3cd018398275a1e47cd96cd2adfa24f5d02db17
Sasha <persa666@yandex.ru>
2023-12-08 16:22:32 +0300

    fix:валидация

--- shareIt-server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ shareIt-server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 /**
@@ -16,15 +14,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookingDto {
-    @Positive
     private int itemId;
 
     @NotNull
-    @FutureOrPresent
     private LocalDateTime start;
 
     @NotNull
-    @FutureOrPresent
     private LocalDateTime end;
 
 }
